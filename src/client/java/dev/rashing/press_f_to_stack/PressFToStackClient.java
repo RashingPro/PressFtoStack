31ad989a59e5cd9f14edeef1a23415179ba529ce
RashingPro <rashingpro@yandex.ru>
2025-12-27 21:08:49 +0300

    Initial commit

---
+++ src/client/java/dev/rashing/press_f_to_stack/PressFToStackClient.java
@@ -0,0 +1,13 @@
+package dev.rashing.press_f_to_stack;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PressFToStackClient implements ClientModInitializer {
+    public static final String MOD_ID = "press-f-to-stack";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitializeClient() {}
+}
